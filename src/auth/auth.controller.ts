cae15f2ce02aa8dfbe4e41c469fe7ef4351142e7
Karel <admin@neo7.fr>
2024-10-27 13:49:54 +0100

    Change loin route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from '../user/entities/user.entity';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
     constructor(private authService: AuthService) { }
 
--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+    // Override the handleRequest method to throw a ForbiddenException instead of UnauthorizedException
     handleRequest(err: any, user: any) {
         if (err || !user) {
             throw new ForbiddenException('Access denied');
